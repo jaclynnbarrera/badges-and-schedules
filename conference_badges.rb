545c933be67b63461f0d1b54a7e8e7a8860bebf4
Jaclyn <jaclynnbarrera@gmail.com>
2021-01-12 20:48:24 -0500

    commit tree e5e92683607a1a63f8d480622e4f3961d9942610

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+    new_list = Array.new
+    arr.each {|name|new_list << "Hello, my name is #{name}."}
+    new_list
+end
+
+def assign_rooms(arr)
+    new_list = Array.new
+    arr.each_with_index {|name,index|new_list << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+    new_list
+end 
+
+def printer(arr)
+    batch_badge_creator(arr).each do |badge|
+      puts badge
+    end
+  
+    assign_rooms(arr).each do |assignment|
+      puts assignment
+    end
+  end
